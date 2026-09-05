1a56e42338df29b0fc71758106f71adcf01a4dd1
ADA <murtag2011@mail.ru>
2026-09-05 09:13:07 +0400

    feat(products): Создать модель поста и зарегистрировать в админке.

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название товара")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    stock = models.IntegerField(default=0, verbose_name="Остаток на складе")
+    is_active = models.BooleanField(default=True, verbose_name="Доступен для продажи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+        ordering = ['-created_at']
+
+
+    def __str__(self):
+        return self.name
