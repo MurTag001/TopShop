2ae145cc7efb6c0d2a33d92feadd70faedb993ee
ADA <murtag2011@mail.ru>
2026-09-09 19:12:19 +0400

    feat(base): Добавить базовый шаблон, создать папку "pages" с переносом страниц

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 def home_page_view (request):
-    return render(request, 'shop/index.html')
+    return render(request, 'shop/pages/index.html')
 
 def product_list_view(request):
     products = Product.objects.filter(is_active=True)
-    return render(request, 'shop/product_list.html', {'products': products})
+    return render(request, 'shop/pages/product_list.html', {'products': products})
 
 
 def shop_detail_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    return render(request, 'shop/product_detail.html', {'product': product})
+    return render(request, 'shop/pages/product_detail.html', {'product': product})
