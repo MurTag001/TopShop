d8cc7ca9eca2ddfb0997f958ebf4f1d2c4a4fe86
ADA <murtag2011@mail.ru>
2026-09-09 21:54:49 +0400

    feat(product/add): Реализовать функционал добавления нового товара.

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Product
 
 def home_page_view (request):
@@ -12,3 +12,20 @@ def product_list_view(request):
 def shop_detail_view(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'shop/pages/product_detail.html', {'product': product})
+
+
+def product_add_view(request):
+    if request.method == "GET":
+        return render(request, 'shop/pages/product_add.html')
+
+    elif request.method == "POST":
+        
+        is_active = 'is_active' in request.POST
+
+        product = Product.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description'],
+            price=request.POST['price'],
+            stock=request.POST['stock']
+        )
+        return redirect('shop:product_detail', product_id=product.id)
